798b20483a85328aada3311418ea9e157c3dc415
Rstar284 <me@rstar.ml>
2022-03-29 12:50:36 +0400

    clean up

--- config/waybar/music.sh
+++ config/waybar/music.sh
@@ -2,21 +2,8 @@
 status=$(spotifycli --playbackstatus)
 song=$(spotifycli --status)
 
-while :
-do
-    if [ "$status" != "$(spotifycli --playbackstatus)" ]
-    then
-        status=$(spotifycli --playbackstatus)
-        song=$(spotifycli --status)
-        echo "$status $song"
-    fi
-    if [ "$song" != "$(spotifycli --status)" ]
-    then
-        status=$(spotifycli --playbackstatus)
-        song=$(spotifycli --status)
-        echo "$status $song"
-    else
-        echo "$status $song"
-    fi
-    sleep 3
-done
+if [ "$status" = "Spotify is off" ]; then
+    echo ""
+else
+    echo "$status $song"
+fi
